1ed37c10947874ff7b8cbf5f2846446c92484f5c
jiang <jiangfz1997@gmail.com>
2026-07-05 23:49:17 -0400

    feat: add GitHub Actions CI/CD, Dockerfiles, and k3s cloud manifests

--- persist_worker_java/src/main/java/com/chatroom/persist/config/DynamoDbConfig.java
+++ persist_worker_java/src/main/java/com/chatroom/persist/config/DynamoDbConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -13,7 +14,7 @@ import java.net.URI;
 @Configuration
 public class DynamoDbConfig {
 
-    @Value("${aws.dynamodb.endpoint}")
+    @Value("${aws.dynamodb.endpoint:}")
     private String endpoint;
 
     @Value("${aws.dynamodb.region}")
@@ -21,11 +22,17 @@ public class DynamoDbConfig {
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
-        return DynamoDbClient.builder()
-                .endpointOverride(URI.create(endpoint))
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("fakeMyKeyId", "fakeSecretAccessKey")))
-                .build();
+        var builder = DynamoDbClient.builder()
+                .region(Region.of(region));
+
+        if (endpoint != null && !endpoint.isBlank()) {
+            builder.endpointOverride(URI.create(endpoint))
+                   .credentialsProvider(StaticCredentialsProvider.create(
+                           AwsBasicCredentials.create("fakeMyKeyId", "fakeSecretAccessKey")));
+        } else {
+            builder.credentialsProvider(DefaultCredentialsProvider.create());
+        }
+
+        return builder.build();
     }
 }
